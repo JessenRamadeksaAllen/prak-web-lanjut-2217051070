34e187dc6ad03b1b31f0c647d29029c18740226e
JessenRamadeksaAllen <chenallen244@gmail.com>
2024-09-24 22:28:05 +0700

    database-model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kelas;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
     public function create(){
-        return view('create_user');
+        return view('create_user', [
+            'kelas' => Kelas::all(),
+        ]);
     }
 
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        $data = [
-            'nama' => $request->input('nama'),
-            'kelas' => $request->input('kelas'),
-            'npm' => $request->input('npm'),
-           ];
-        return view('profile', $data);
+        $validatedData = $request->validate([
+            'nama' => 'required|string|max:255',
+            'npm' => 'required|string|max:255',
+            'kelas_id' => 'required|exists:kelas,id',
+           ]);
+
+           $user = UserModel::create($validatedData);
+           
+           $user->load('kelas');
+
+        return view('profile', [
+            'nama' => $user->nama,
+            'npm' => $user->npm,
+            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+            
+        ]);
     }
 }
---
+++ database/seeders/KelasSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kelas;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class KelasSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        $data = [
+            'A',
+            'B',
+            'C',
+            'D',
+        ];
+
+        foreach ($data as $kelas){
+            Kelas::create([
+                'nama_kelas' => $kelas,
+            ]);
+        }
+    }
+}
