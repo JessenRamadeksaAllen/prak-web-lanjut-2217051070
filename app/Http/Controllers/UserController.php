b1caf1ecbc22783abf813d1bab10ed0704caf46e
JessenRamadeksaAllen <chenallen244@gmail.com>
2024-10-11 12:49:45 +0700

    tes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,93 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Kelas;
 use App\Models\UserModel;
-use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
-    public $userModel;
-    public $kelasModel;
-
-    public function __construct()
+    public function index()
     {
-        $this->userModel = new UserModel();
-        $this->kelasModel = new Kelas();
-    }
-
-    public function index() 
-    { 
-        $users = $this->userModel->getUser(); // Ambil data user dari model
-        
-        $data = [ 
-            'title' => 'List of Users', 
-            'users' => $users, // Kirim variabel $users ke view
-        ]; 
-
-        return view('list_user', $data); 
-    }
-
-
-    // public function index() 
-    // { 
-    //     $data = [ 
-    //         'title' => 'Create User', 
-    //         'kelas' => $this->userModel->getUser(), 
-    //     ]; 
+        $data = [
+            'title' => 'List User',
+            'users' => $this->userModel->getUser(),
+        ];
     
-    //     return view('list_user', $data); 
-    // }
+        return view('list_user', $data);
+    }
 
-    public function create(){
+    public function profile($nama = "", $kelas = "", $npm = ""){
+        $data = [
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'npm' => $npm,
+        ];
+        
+        return view('profile', $data);
+    }
 
-        $kelas = $this->kelasModel->getKelas();
+    public function create()
+    {
+        $kelas = new Kelas();
 
-        // $kelasModel = new Kelas ();
-
-        // $kelas = $kelasModel->getKelas();
+        $kelas = $kelas->getKelas();
 
         $data = [
             'title' => 'Create User',
             'kelas' => $kelas,
         ];
-
+        
         return view('create_user', $data);
-        // return view('create_user', [
-        //     'kelas' => Kelas::all(),
-        // ]);
     }
 
-    public function store(Request $request) 
-    { 
+    public function store(Request $request)
+    {
         $this->userModel->create([ 
-        'nama' => $request->input('nama'), 
-        'npm' => $request->input('npm'), 
-        'kelas_id' => $request->input('kelas_id'), 
-        ]); 
-        return redirect()->route('users.index'); 
+            'nama' => $request->input('nama'), 
+            'npm' => $request->input('npm'), 
+            'kelas_id' => $request->input('kelas_id'), 
+            ]); 
+        
+            return redirect()->to('/user');
     }
+    public $userModel;
+    public $kelasModel;
 
-    // public function store(UserRequest $request)
-    // {
-    //     $validatedData = $request->validate([
-    //         'nama' => 'required|string|max:255',
-    //         'npm' => 'required|string|max:255',
-    //         'kelas_id' => 'required|exists:kelas,id',
-    //        ]);
-
-    //        $user = $this->userModel->create($validatedData);
-
-    //     //    $user = UserModel::create($validatedData);
-           
-    //        $user->load('kelas');
-
-    //     return view('profile', [
-    //         'nama' => $user->nama,
-    //         'npm' => $user->npm,
-    //         'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
-            
-    //     ]);
-    // }
+    public function __construct()
+    {
+        $this->userModel = new UserModel;
+        $this->kelasModel = new Kelas();
+        $kelas = $this->kelasModel->getKelas();
+        
+    }
 }
